4e33c402d53cbef6c2a0de3c0f97824ec0d31084
Prashanta D Acharya <apras.dixit@gmail.com>
2024-08-02 12:23:42 +0545

    Added Rooms component in Home

--- src/pages/Home/OurRooms.jsx
+++ src/pages/Home/OurRooms.jsx
@@ -1,4 +1,7 @@
 import SectionHeader from "../../components/SectionHeader";
+import RoomCard from "../../components/RoomCard";
+import { roomItems } from "../../constants/constant";
+import { Link } from "react-router-dom";
 
 const OurRooms = () => {
 	return (
@@ -6,9 +9,33 @@ const OurRooms = () => {
 			<SectionHeader
 				title="Our Rooms"
 				subtitle="Explore Our"
-				section="Rooms"
+				section="Featured Rooms"
 				color="primary"
 			/>
+
+			<div className="flex flex-wrap xs:justify-center lg:justify-evenly items-center gap-10 my-16">
+				{roomItems.map((item, index) => (
+					<RoomCard
+						key={index}
+						img={item.img}
+						name={item.name}
+						star={item.star}
+						details={item.details}
+						desc={item.desc}
+						btn1={item.btn1}
+						btn2={item.btn2}
+						price={item.price}
+					/>
+				))}
+			</div>
+			<Link
+				to={"/rooms"}
+				className="flex items-center justify-center my-12"
+			>
+				<button className="uppercase bg-extra hover:bg-dark text-normal hover:text-primary font-bold py-4 px-10 rounded focus:outline-none focus:shadow-outline transition-all duration-300">
+					View All
+				</button>
+			</Link>
 		</section>
 	);
 };
--- src/pages/Home/OurServices.jsx
+++ src/pages/Home/OurServices.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import SectionHeader from "../../components/SectionHeader";
+import ServicesCard from "../../components/ServicesCard";
+import { services } from "../../constants/constant";
 
 const OurServices = () => {
 	return (
@@ -9,6 +12,24 @@ const OurServices = () => {
 				section="Services"
 				color="extra"
 			/>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-16 place-items-center ">
+				{services.slice(0, 3).map((item, index) => (
+					<ServicesCard
+						key={index}
+						icon={item.icon}
+						name={item.name}
+						desc={item.desc}
+					/>
+				))}
+			</div>
+			<Link
+				to={"/services"}
+				className="flex items-center justify-center my-12"
+			>
+				<button className="uppercase bg-extra hover:bg-dark text-normal hover:text-primary font-bold py-4 px-10 rounded focus:outline-none focus:shadow-outline transition-all duration-300">
+					View All
+				</button>
+			</Link>
 		</section>
 	);
 };
